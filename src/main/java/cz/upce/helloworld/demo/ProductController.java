47f8ce9281475e8dce347b2e72f5e0133ba8ea81
Petr Filip <petr.filip@tix.cz>
2020-04-20 10:05:39 +0200

    Updated tests

--- src/main/java/cz/upce/helloworld/demo/ProductController.java
+++ src/main/java/cz/upce/helloworld/demo/ProductController.java
@@ -2,13 +2,16 @@ package cz.upce.helloworld.demo;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
-  
+  Logger logger = LoggerFactory.getLogger(ProductController.class);
+
   private final ProductService productService;
 
   public ProductController(ProductService productService) {
@@ -17,6 +20,7 @@ public class ProductController {
 
   @GetMapping("/product")
   public List<ProductModel> listProducts() {
+    logger.info("List product endpoint has been touched!");
     return productService.listProducts();
   }
 
--- src/test/java/cz/upce/helloworld/demo/DemoApplicationTests.java
+++ src/test/java/cz/upce/helloworld/demo/DemoApplicationTests.java
@@ -1,13 +1,25 @@
 package cz.upce.helloworld.demo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("heroku")
 class DemoApplicationTests {
+
+  @Autowired
+  private ProductServiceHeroku productService;
 
   @Test
   void contextLoads() {
+    List<ProductModel> productModels = productService.listProducts();
+    assertThat(productService).isNotNull();
+    System.out.println(productModels.size());
   }
 
 }
